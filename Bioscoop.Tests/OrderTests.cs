0a59f8a5669f593863dc40ceefa4d68b5a071757
PeterVb12 <peter-veter@outlook.com>
2026-02-08 22:53:50 +0100

    tests added + yml

---
+++ Bioscoop.Tests/OrderTests.cs
@@ -0,0 +1,25 @@
+﻿using Bioscoop.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bioscoop.Tests
+{
+    public class OrderTests
+    {
+        [Fact]
+        public void Order_ShouldInitializeWithCorrectOrderNumber()
+        {
+            // Arrange
+            int expectedOrderNr = 123;
+
+            // Act
+            Order order = new Order(expectedOrderNr, false);
+
+            // Assert
+            Assert.Equal(expectedOrderNr, order.getOrderNr());
+        }
+    }
+}
